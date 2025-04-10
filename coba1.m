cdecfa23f80f6d9578174fb997b07205e8a0c4f0
kristichristie81@gmail.com <kristichristie81@gmail.com>
2025-04-10 10:03:54 +0700

    KomputasiMatematika

---
+++ coba1.m
@@ -0,0 +1,17 @@
+clc
+clear
+
+% menghitung luas persegi panjang(memberikan komentar)
+p = 10;
+l = 20;
+lp = p*l;
+% disp(lp)
+
+
+lp1 = p+1;
+lp2 = p-l;
+lp3 = p/1;
+
+disp(lp1)
+disp(lp2)
+disp(lp3)
