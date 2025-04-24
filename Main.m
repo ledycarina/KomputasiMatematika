283d403f47c585b66203553d1b113ced65589dec
kristichristie81@gmail.com <kristichristie81@gmail.com>
2025-04-24 21:57:33 +0700

    KomputasiMatematika

---
+++ Main.m
@@ -0,0 +1,15 @@
+clc
+clear
+
+%Definisikan fungsi
+f = inline('x^2 - 3', 'x');
+
+%Interval [1,2]
+X = [1 2];
+
+%Panggil fungsi BagiDua
+[x,galat] = bagidua_1(f,X);
+disp('akar hampiran =')
+disp(x)
+disp('galat =')
+disp(galat)
